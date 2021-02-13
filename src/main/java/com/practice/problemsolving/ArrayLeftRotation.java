0c17fc0be210b9ad7b446f17c82737a4eee5428b
prakash <bdkpreddy@gmail.com>
2021-02-13 22:51:43 +0530

    18-02-21 ArrayLeftRotation and SortArrayWithMinimumSwap problems

---
+++ src/main/java/com/practice/problemsolving/ArrayLeftRotation.java
@@ -0,0 +1,9 @@
+package com.practice.problemsolving;
+
+/**
+ * LeftRotation: Left rotation means moving the arrays first element to last position.
+ * Ex: Input: {1,2,3,4,5}, no.of Rotations: 3
+ *     Output: Return the result rotated array {4,5,1,2,3}
+ * */
+public class ArrayLeftRotation {
+}
---
+++ src/main/java/com/practice/problemsolving/SortArrayWithMinimumSwap.java
@@ -0,0 +1,15 @@
+package com.practice.problemsolving;
+
+/**
+ * Input: Given a array with numbers from {1-n} (Here n is the size of array) in random order.
+ * Constraint: In each step you are allowed to swap any two numbers.
+ * Find minimum steps required to sort the array in ascending order.
+ * Ex: array : {5,3,4,1,2}
+ *             {2,3,4,1,5}  Swap(0,4)
+ *             {3,2,4,1,5}  Swap(0,1)
+ *             {4,2,3,1,5}  Swap(0,2)
+ *             {1,2,3,4,5}  Swap(0,3)
+ *     Result : 4 steps
+ * */
+public class SortArrayWithMinimumSwap {
+}
