570ee0a394fef8d8cc3a2647b347b363f7398e13
prakash <bdkpreddy@gmail.com>
2021-02-13 22:30:33 +0530

    MergeTwoSortedArrays

---
+++ src/main/java/com/practice/problemsolving/MergeTwoSortedArrays.java
@@ -0,0 +1,8 @@
+package com.practice.problemsolving;
+
+/**
+ * Input: Two sorted arrays : array1 = {3,5,6,10}, array2 = {2,5,15}
+ * Output: {2,3,5,6,10,15}
+ * */
+public class MergeTwoSortedArrays {
+}
